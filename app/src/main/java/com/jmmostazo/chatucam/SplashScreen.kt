0e1501009f2122109e8385a61dda6f43c071f465
Jorge Martínez <jmmostazo.medac@gmail.com>
2023-10-23 19:31:38 +0200

    Initial commit

---
+++ app/src/main/java/com/jmmostazo/chatucam/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.jmmostazo.chatucam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        MostrarBienvenida()
+    }
+
+    fun MostrarBienvenida(){
+        //Permite ejecutar un codigo pasado cierto tiempo
+        object : CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {
+
+            }
+            //Una vez finalizado el tiempo nos dirigimos al Activity principal
+            override fun onFinish() {
+                val intent = Intent(applicationContext,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.start()
+    }
+}
