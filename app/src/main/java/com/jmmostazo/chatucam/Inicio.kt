382b884313249588b6c33701fd17b7157fcdd966
Jorge Martínez <jmmostazo.medac@gmail.com>
2023-10-28 11:30:17 +0200

    Cerrar sesión

--- app/src/main/java/com/jmmostazo/chatucam/Inicio.kt
+++ app/src/main/java/com/jmmostazo/chatucam/Inicio.kt
@@ -11,12 +11,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Inicio : AppCompatActivity() {
 
     private lateinit var Btn_ir_registros : Button
     private lateinit var Btn_ir_logueo : Button
 
+    //variable de usuario de firebase para el login
+    var firebaseUser : FirebaseUser?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
@@ -24,22 +28,39 @@ class Inicio : AppCompatActivity() {
         //Instanciamos los dos botones obteniendo su id
         Btn_ir_registros = findViewById(R.id.Btn_ir_registros)
         Btn_ir_logueo = findViewById(R.id.Btn_ir_logueo)
+
         //Evento cuando pulsamos el botón Registro
         Btn_ir_registros.setOnClickListener {
             val intent = Intent(this@Inicio, RegistroActivity::class.java)
-            Toast.makeText(applicationContext,"Registros",Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext,"Registro de usuario",Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
 
         //Evento cuando pulsamos el botón Registro
         Btn_ir_logueo.setOnClickListener {
             val intent = Intent(this@Inicio, RegistroActivity::class.java)
-            Toast.makeText(applicationContext,"Login",Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext,"Login de usuario",Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
 
 
     }
+    //En esta función vamos a comprobar el login de usuario en Firebase.
+    //Si existe el usuario nos vamos a la actividad principal
+    private fun ComprobarSesion(){
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        if (firebaseUser!=null){
+            val intent = Intent(this@Inicio, MainActivity::class.java)
+            Toast.makeText(applicationContext,"Sesión Activa",Toast.LENGTH_LONG).show()
+            startActivity(intent)
+            finish()
+        }
+    }
 
+    //Cuando inicie la aplicación comprobará en Firebase si la sesión de usuario está activa
+    override fun onStart(){
+        ComprobarSesion()
+        super.onStart()
+    }
 
 }
--- app/src/main/java/com/jmmostazo/chatucam/MainActivity.kt
+++ app/src/main/java/com/jmmostazo/chatucam/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.jmmostazo.chatucam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //Con esta función creamos las opciones de menu y visualizar el menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_principal,menu)
+        return true
+    }
+
+    //Esta función permite seleccionar una opción del menú
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Si seleccionamos un item y es Cerrar sesión
+        return when(item.itemId) {
+            R.id.menu_salir->{
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this@MainActivity, Inicio::class.java)
+                startActivity(intent)
+                return true
+            }
+            //En caso de no seleccionar item Cerrar sesión
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
