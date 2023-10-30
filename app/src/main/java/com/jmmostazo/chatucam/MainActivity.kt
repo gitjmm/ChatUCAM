096797cdda91be94ffcca4be6274155141d20591
Jorge Martínez <jmmostazo.medac@gmail.com>
2023-10-30 09:01:06 +0100

    Menu y nombre usuario

--- app/src/main/java/com/jmmostazo/chatucam/MainActivity.kt
+++ app/src/main/java/com/jmmostazo/chatucam/MainActivity.kt
@@ -7,27 +7,57 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.jmmostazo.chatucam.fragmentos.FragmentoChats
 import com.jmmostazo.chatucam.fragmentos.FragmentoUsuarios
+import com.jmmostazo.chatucam.model.Usuario
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var Btn_cerrar_sesion : Button
+    //private lateinit var Btn_cerrar_sesion : Button
+    //Referencia a la base de datos firebase
+    var reference : DatabaseReference?=null
+    //usuario actual
+    var firebaseuser : FirebaseUser?=null
+    //Inicializamos TextView donde se va a mostrar el nombre de usuario conectado en tiempo real
+    private lateinit var nombre_usuario : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //FirebaseAuth.getInstance().signOut()
+        //Esta función inicializa todos los componentes que se muestran en la actividad principal
         InicializarComponentes()
+        //Esta función obtiene el nombre de usuario de la base de datos y lo muestra en TextView
+        ObtenerDato()
     }
 
     //Esta función inicializa todos los componentes
     fun InicializarComponentes(){
+
+        val toolbar : Toolbar = findViewById(R.id.Toolbar_main)
+        setSupportActionBar(toolbar)
+
+        //Obtenemos el usuario de la base de datos
+        firebaseuser = FirebaseAuth.getInstance().currentUser
+        //Obtenemos la referencia a la base de datos
+        reference = FirebaseDatabase.getInstance().reference.child("chatUCAM").child(firebaseuser!!.uid)
+        //Obtenemos referencia a textview nombre de usuario
+        nombre_usuario = findViewById(R.id.Nombre_usuario)
+
         val tabLayout : TabLayout = findViewById(R.id.TabLayoutMain)
         val viewPager : ViewPager = findViewById(R.id.ViewPagerMain)
 
@@ -39,6 +69,25 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = viewpagerAdapter
         tabLayout.setupWithViewPager(viewPager)
     }
+
+    //Con esta función obtenemos dato nombre de usuario en tiempo real
+    fun ObtenerDato(){
+        reference!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot){
+               //Si el usuario existe recuperamos su valor y lo incluimos en el TextView
+                if(snapshot.exists()){
+                    val usuario : Usuario? = snapshot.getValue(Usuario::class.java)
+                    nombre_usuario.text = usuario!!.getN_Usuario()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+
 
     //Creamos una clase para inicializar el elemento ViewPager
     class ViewPagerAdapter(fragmentManager : FragmentManager): FragmentPagerAdapter(fragmentManager) {
---
+++ app/src/main/java/com/jmmostazo/chatucam/model/Usuario.kt
@@ -0,0 +1,127 @@
+package com.jmmostazo.chatucam.model
+
+//Clase usuario correspondiente al usuario de firebase
+
+class Usuario {
+    private var uid : String = ""
+    private var nom_usuario : String = ""
+    private var email : String = ""
+    private var telefono : String = ""
+    private var imagen : String = ""
+    private var buscar : String = ""
+    private var nombre : String = ""
+    private var apellido : String = ""
+    private var edad : String = ""
+    private var profesion : String = ""
+    private var domicilio : String = ""
+
+    constructor()
+
+    constructor(
+        uid: String,
+        nom_usuario: String,
+        email: String,
+        //telefono: String,
+        imagen: String,
+        buscar: String,
+        nombre: String,
+        apellido: String,
+        edad: String,
+        profesion: String,
+        domicilio: String
+    ) {
+        this.uid = uid
+        this.nom_usuario = nom_usuario
+        this.email = email
+        //this.telefono = telefono
+        this.imagen = imagen
+        this.buscar = buscar
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.profesion = profesion
+        this.domicilio = domicilio
+    }
+
+    //getters y setters
+    fun getUid() : String?{
+        return uid
+    }
+
+    fun setUid(uid : String){
+        this.uid = uid
+    }
+
+    fun getN_Usuario() : String?{
+        return nom_usuario
+    }
+
+    fun setN_Usuario(n_usuario : String){
+        this.nom_usuario = n_usuario
+    }
+
+    fun getEmail() : String?{
+        return email
+    }
+
+    fun setEmail(email : String){
+        this.email = email
+    }
+
+
+    fun getImagen() : String?{
+        return imagen
+    }
+
+    fun setImagen(imagen : String){
+        this.imagen = imagen
+    }
+
+    fun getBuscar() : String?{
+        return buscar
+    }
+
+    fun setBuscar(buscar : String){
+        this.buscar = buscar
+    }
+
+    fun getNombre() : String?{
+        return nombre
+    }
+
+    fun setNombre(nombres : String){
+        this.nombre = nombres
+    }
+
+    fun getApellido() : String?{
+        return apellido
+    }
+
+    fun setApellido(apellidos : String){
+        this.apellido = apellidos
+    }
+
+    fun getEdad() : String?{
+        return edad
+    }
+
+    fun setEdad(edad : String){
+        this.edad = edad
+    }
+
+    fun getProfesion() : String?{
+        return profesion
+    }
+
+    fun setProfesion(profesion : String){
+        this.profesion = profesion
+    }
+
+    fun getDomicilio() : String?{
+        return domicilio
+    }
+
+    fun setDomicilio(domicilio : String){
+        this.domicilio = domicilio
+    }
+}
